17839e42e47a1cfa6566c78b0c4ee613f73980ae
Duy Lai <duyklai@gmail.com>
2019-03-26 13:04:18 -0500

    pushing WIP for 3/26

---
+++ GameArt/Assets/Scripts/Detection/NewPlayer.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewPlayer : MonoBehaviour
+{
+    public GameObject sun;
+
+    private MeshRenderer mesh;
+    private RaycastHit hit;
+    private bool underSun = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        underSun = false;
+        Vector3 sunDir = sun.transform.forward;
+        sunDir.Normalize();
+        sunDir *= 20;
+
+        //foreach ()
+        //{
+            if (!Physics.Raycast(transform.position, -1f * sunDir, 30, LayerMask.GetMask("Ground")))
+            {
+                Debug.DrawLine(transform.position, transform.position - sunDir, Color.red);
+                underSun = true;
+            }
+            else
+            {
+                Debug.DrawLine(transform.position, transform.position - sunDir, Color.green);
+            }
+
+        //}
+
+        if (underSun)
+        {
+            mesh.material.color = Color.red;
+        }
+        else
+        {
+            mesh.material.color = Color.green;
+        }
+    }
+}
+
+
--- GameArt/Assets/Scripts/Trap/pit.cs
+++ GameArt/Assets/Scripts/Trap/pit.cs
@@ -2,23 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pit : MonoBehaviour {
+public class pit : MonoBehaviour
+{
 
     private Health playerHealth;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         playerHealth = GameObject.Find("Player(Cube)").GetComponent<Health>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             Debug.Log("Hit");
             playerHealth.takeDamage(playerHealth.maxHealth);
